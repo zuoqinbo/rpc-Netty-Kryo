02fabf953b1cd6cd41c20a41973d0932eaa87657
zuo_qin_bo@163.com <Zuoqinbo51>
2019-07-24 21:13:43 +0800

    dev commit;

---
+++ src/main/java/com/maigo/rpc/InvokeClient.java
@@ -0,0 +1,63 @@
+package com.maigo.rpc;
+
+import com.maigo.rpc.client.RpcClientAsyncProxy;
+import com.maigo.rpc.client.RpcClientProxyBuilder;
+import com.maigo.rpc.future.RpcFuture;
+import com.maigo.rpc.inter.CustomException;
+import com.maigo.rpc.inter.CustomObject;
+import com.maigo.rpc.inter.UserService;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+/**
+ * InvokClient
+ *
+ * @author ZuoQinbo
+ * @date 2019/7/24
+ */
+public class InvokeClient {
+
+    public static void main(String[] args) {
+
+        RpcClientAsyncProxy rpcClientAsyncProxy =  RpcClientProxyBuilder.create(UserService.class)
+                .timeout(2000)
+                .threads(4)
+                .connect("127.0.0.1", 3721)
+                .buildAsyncProxy();
+        RpcFuture rpcFuture = rpcClientAsyncProxy.call("methodWithoutArg");
+
+//        try
+//        {
+//            rpcFuture.get();
+//        }
+//        catch (Throwable e)
+//        {
+//            if(e instanceof CustomException)
+//                return;
+//
+//            fail(e + " was caught when testFutureError.");
+//        }
+//
+//        fail("failed to catch JUnitTestCustomException.");
+        UserService userService = RpcClientProxyBuilder.create(UserService.class)
+                .timeout(2000)
+                .threads(4)
+                .connect("127.0.0.1", 3721)
+                .build();
+
+
+        CustomObject beforeCustomObject = new CustomObject("before", 3);
+        CustomObject afterCustomObject =
+                userService.methodWithCustomObject(beforeCustomObject);
+        assertEquals("before after", afterCustomObject.getString());
+
+        assertEquals(50, afterCustomObject.getI());
+
+        List<String> list = userService.methodReturnList("hello", "world");
+
+        System.out.println(list.get(0));
+    }
+}
